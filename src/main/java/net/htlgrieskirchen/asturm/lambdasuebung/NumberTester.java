a41f246e93d3491ccb01e74136a37262fe691db5
Andreas Sturm <Andi_Sturm@gmx.at>
2019-09-28 10:46:34 +0200

    Debugging

--- src/main/java/net/htlgrieskirchen/asturm/lambdasuebung/NumberTester.java
+++ src/main/java/net/htlgrieskirchen/asturm/lambdasuebung/NumberTester.java
@@ -9,19 +9,25 @@ import java.io.*;
 public class NumberTester 
 {
 
-    int numberOfCommandLines; //First Line of the txt File will be stored in here. 
+    public static int numberOfCommandLines; //First Line of the txt File will be stored in here. 
+    public static String[] commandsAsStrings; //The Following lines will be stored in here before they get split.
     
     public NumberTester(String fileName) throws FileNotFoundException, IOException
     {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-            //fileName would be: " C:\\Users\\user\\Documents\\NetBeansProjects\\asturm_LambdasUebung\\numbertester_input.txt "
+        /*BufferedReader br = new BufferedReader(new FileReader(fileName));
+            //fileName would be: 
         numberOfCommandLines = Integer.parseInt(br.readLine());
         
         for(int i = 0; i<=numberOfCommandLines; i++)
         {
             
+            if(i != 0)
+            {
+                commandsAsStrings[i] = br.readLine();
+                System.out.println(commandsAsStrings[i]);
+            }
             
-        }
+        }*/
         
     }
     
@@ -58,6 +64,25 @@ public class NumberTester
     {
         
         //for tests
+        
+        
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\user\\Documents\\NetBeansProjects\\asturm_LambdasUebung\\numbertester_input.txt"));
+            //fileName would be: " C:\\Users\\user\\Documents\\NetBeansProjects\\asturm_LambdasUebung\\numbertester_input.txt "
+        numberOfCommandLines = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i<=numberOfCommandLines; i++)
+        {
+            
+            if(i != 0)
+            {
+                //commandsAsStrings[i] = br.readLine();
+                //System.out.println("Im Array " +commandsAsStrings[i]);
+                System.out.println(br.readLine());
+            }
+         
+            
+            
+        }
         
     }
 }
